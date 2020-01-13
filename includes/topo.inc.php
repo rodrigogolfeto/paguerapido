9032768e1b55036e3f85eb737d4593af5a7dfa62
Rodrigo Golfeto <rgolfeto@gmail.com>
2020-01-13 07:27:46 -0400

    Ajuste no espaçamento do topo no mobile

--- includes/topo.inc.php
+++ includes/topo.inc.php
@@ -22,7 +22,7 @@
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="collapse navbar-collapse pb-3 pb-sm-0" id="navbarSupportedContent">
                         
                         <div class="d-flex justify-content-between flex-column flex-column-reverse flex-sm-row  w-100">
 
